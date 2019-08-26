5cb4d8904839b02d29f4c129c264419483d5a089
A-Chidalu <50505942+A-Chidalu@users.noreply.github.com>
2019-08-25 20:11:22 -0400

    Update send_whatsapp_media.py

--- send_whatsapp_media.py
+++ send_whatsapp_media.py
@@ -2,9 +2,9 @@ from twilio.rest import Client
 
 
 # Your Account SID from twilio.com/console
-account_sid = "AC05c0692e167c2f177d35ebb0a2662f84"
+account_sid = "SOME_AUTH_SID"
 # Your Auth Token from twilio.com/console
-auth_token = "fd2ee28ac05d4310c83d4ae68b5f6e8a"
+auth_token = "SOME_AUTH_TOKEN"
 
 client = Client(account_sid, auth_token)
 
